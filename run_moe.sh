5ec511a176799ab2a418b0dd03ffa83c4f29a7cf
tomerporian <tomerpor@gmail.com>
2025-11-06 12:44:46 +0100

    config files

--- run_moe.sh
+++ run_moe.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --account=projectnucleus
+#SBATCH --account=laionize
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=4
 #SBATCH --gpus-per-node=4
@@ -13,9 +13,15 @@
 #SBATCH --exclude=jwb[0026,0098,0193,0631,0731,0729,0801,0807,0833,0964,1021,0908,0726,0309,0234,0095,0059,0199,0745,0132,0250,0636,0821,0633,0216,0921,0832,0295,0091,0133,0319,0294,0385,0384,0093,1126,0485,0286,0007,0681,0182,0802,0221,0476,0254,0898]
 
 # --- Environment Setup ---
+export CONF=$1
 export WANDB_MODE=offline
-export PATH="$HOME/.local/bin:$PATH"
+module load CUDA/12
 source /p/data1/mmlaion/porian1/.venv/bin/activate
+export TORCH_CUDNN_SDPA_ENABLED=1  # Enable cuDNN attention backend
+export MPLCONFIGDIR=/p/data1/mmlaion/porian1/.cache/matplotlib  # Matplotlib config directory
+
+# --- Triton/CUDA compilation settings ---
+export TRITON_CACHE_DIR=/p/data1/mmlaion/porian1/.triton_cache/cache
 
 # --- Networking for Multi-Node Communication ---
 export NCCL_SOCKET_IFNAME=ib0
@@ -44,7 +50,7 @@ srun --export=ALL bash -c '
     export LOCAL_RANK=$SLURM_LOCALID
     export WORLD_SIZE=$SLURM_NTASKS
     echo "Process $RANK on $(hostname): RANK=$RANK, LOCAL_RANK=$LOCAL_RANK, WORLD_SIZE=$WORLD_SIZE"
-    python train_gpt_moe.py
+    python train_gpt_moe.py --config $CONF
 '
 
 echo "Job finished."
